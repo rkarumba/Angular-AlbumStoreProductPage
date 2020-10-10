4a4ddb6458c06cb7d5db5265795cced3b136c502
Raymond Karumba N <karumbaraymond@gmail.com>
2020-10-10 15:54:23 +0300

    Displaying a List of Products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,9 +12,11 @@ import { ProductService } from '../product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[]
-  constructor(private _productService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getProducts().subscribe(response =>
+      this.products = response);
   }
 
 }
