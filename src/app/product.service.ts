16379b36797f56250a280cb36e3da1c943781cbd
Raymond Karumba N <karumbaraymond@gmail.com>
2020-10-10 15:58:03 +0300

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,7 +14,7 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json'
 
-  private _productUrl = '../assets/products.json'
+  private _productsUrl = '../assets/products.json'
 
   constructor(private _http: Http) { }
 
@@ -24,7 +24,7 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]>{
-    return this._http.get(this._productUrl).map(response =>
+    return this._http.get(this._productsUrl).map(response =>
       <Product[]>response.json());
   }
 
