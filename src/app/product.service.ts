ccc4c0781f142bbd304cb9bfbe5b4ea03e69ae38
Raymond Karumba N <karumbaraymond@gmail.com>
2020-10-10 11:21:52 +0300

    Creating a Service to Get Album Data

---
+++ src/app/product.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+
+}
